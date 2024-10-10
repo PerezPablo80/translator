276e7e3eabdb60e87f7b42c6fb9039ff7bd81d6a
perezpablo80 <perez.pablo@gmail.com>
2024-10-10 16:18:04 -0300

    proxy 4

--- api/translate.js
+++ api/translate.js
@@ -4,7 +4,7 @@ export default async function translate(req, res) {
 	try {
 		const { text, to } = req.query; // receive text and language as query params
 		const result = await googleTranslate(text, { to });
-		let resul = { result: result, resultado: "prueba de resultado" };
+		let resul = { result: json(result), resultado: "prueba de resultado" };
 		res.setHeader("Access-Control-Allow-Origin", "*"); // Allow CORS
 		res.status(200).json(resul);
 	} catch (error) {
