4860cfd022ebe8a087a2f8833b9c6db1852d4aea
perezpablo80 <perez.pablo@gmail.com>
2024-10-10 16:16:20 -0300

    proxy try 3

--- api/translate.js
+++ api/translate.js
@@ -1,14 +1,12 @@
 import googleTranslate from "google-translate-api-x";
 
-export default async function handler(req, res) {
-	console.log("access to function handler");
+export default async function translate(req, res) {
 	try {
 		const { text, to } = req.query; // receive text and language as query params
-		console.log(`on translate.js: text:${text} and to:${to}`);
 		const result = await googleTranslate(text, { to });
-		console.log("result:::::::", result);
+		let resul = { result: result, resultado: "prueba de resultado" };
 		res.setHeader("Access-Control-Allow-Origin", "*"); // Allow CORS
-		res.status(200).json(result);
+		res.status(200).json(resul);
 	} catch (error) {
 		res.status(500).json({ error: error.message });
 	}
